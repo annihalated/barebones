714e10172fd8af8c1b1ee4c6fb6c8bce71303f55
Nihal Sahu <iamnihalsahu@gmail.com>
2024-12-30 15:17:28 +0530

    Add RSS Feed

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,27 @@
 import markdownIt from "markdown-it";
-import markdownItFootnote from "markdown-it-footnote"
+import markdownItFootnote from "markdown-it-footnote";
+import { feedPlugin } from "@11ty/eleventy-plugin-rss";
 
 export default function(eleventyConfig) {
+  	eleventyConfig.addPlugin(feedPlugin, {
+		type: "rss",
+		outputPath: "/feed.xml",
+		collection: {
+			name: "post", // iterate over `collections.posts`
+			limit: 0,     // 0 means no limit
+		},
+		metadata: {
+			language: "en",
+			title: "nihalsahu.net",
+			subtitle: "Nihal Sahu's personal website.",
+			base: "https://nihalsahu.net/",
+			author: {
+				name: "Nihal Sahu",
+				email: "iamnihalsahu@gmail.com", // Optional
+			}
+		}
+	});
+
   let options = {
 		html: true,
 		breaks: true,
